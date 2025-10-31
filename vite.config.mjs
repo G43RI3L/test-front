b51eef858ca4d27e2978c2402ff065cf2ac7cdc9
unknown <ghmbinvest@gmail.com>
2025-10-31 14:06:37 -0300

    aterials

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/test-front/", // 👈 obrigatório no GitHub Pages
+  base: "/test-front/", // obrigatório para GitHub Pages
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
